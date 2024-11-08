ec9439bb08f14ecb063fba18357c2552ef66780f
Nguyen Hoang Anh <anhnhph49560@gmail.com>
2024-11-09 00:09:45 +0700

    Lab 2

---
+++ server_express.js
@@ -0,0 +1,20 @@
+const { log } = require('console')
+const express = require('express')
+const app = express()
+const port = 3003
+
+app.get('/', (req, res) => {
+    console.log("Vao trang chu");
+    
+  res.send('Trang chu')
+})
+
+app.get('/home', (req, res) => {
+    console.log("Vao trang home");
+    
+    res.send('Home')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
